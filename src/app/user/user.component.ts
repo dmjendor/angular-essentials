c493aaa643b3dbe8e9229c5e707b4ccf0802f846
dmjendor@gmail.com <dmjendor@gmail.com>
2025-11-30 16:51:39 -0500

    Add Input Signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +9,17 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
   // ! tells typescript the value will be set outside.
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  // Input signals are read only.
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar();
+  // }
+  imagePath = computed(() => 'assets/users/' + this.avatar());
 
   onSelectUser() {}
 }
