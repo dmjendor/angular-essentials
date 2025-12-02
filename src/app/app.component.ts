2d0706cac2bc52d9c9653efc330aac5e8e4cd732
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-02 12:02:02 -0500

    Convert all standalone components to modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { DUMMY_TASKS } from './dummy-tasks';
 import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
